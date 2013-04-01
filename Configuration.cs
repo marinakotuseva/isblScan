ea0024644d4791b3d88ba464e7a537a427e5dd8c
polarnik <unknown-user@192.168.0.2>
2013-04-01 22:50:41 +0400

    0.0.5.2

---
+++ Configuration.cs
@@ -0,0 +1,60 @@
+
+using System;
+
+namespace isblTest
+{
+
+	/// <summary>
+	/// Класс для хранения конфигурации программы
+	/// </summary>
+	public static class Configuration
+	{
+		private static string nameSqlServer = "Sql Server:";
+		private static string nameDataBase = "Data Base:";
+		private static string nameLogin = "Login:";
+		
+		private static string configFilePath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData) + "/isblTest.cfg";
+		
+
+		public static bool Load(out string sqlServer, out string dataBase, out string login)
+		{
+			sqlServer = "";
+			dataBase = "";
+			login = "";
+			if(System.IO.File.Exists(configFilePath))
+			{
+				string[] configStrings = System.IO.File.ReadAllLines(configFilePath);
+				foreach(string configString in configStrings)
+				{
+					if(configString.StartsWith(nameSqlServer))
+					{
+						sqlServer = configString.Remove(0, nameSqlServer.Length);
+					}
+					if(configString.StartsWith(nameDataBase))
+					{
+						dataBase = configString.Remove(0, nameDataBase.Length);
+					}
+					if(configString.StartsWith(nameLogin))
+					{
+						login = configString.Remove(0, nameLogin.Length);
+					}
+				}
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		
+		public static void Save(string sqlServer, string dataBase, string login)
+		{
+			System.IO.File.WriteAllText(configFilePath,
+			                            String.Format("{0}{1}\n{2}{3}\n{4}{5}",
+			                                          nameSqlServer, sqlServer,
+			                                          nameDataBase, dataBase,
+			                                          nameLogin, login));
+			                                                
+		}
+	}
+}
---
+++ SynchronizedScrollRichTextBox.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+/*
+ * Use code from
+ * https://gist.github.com/593809
+ * bhttp://stackoverflow.com/questions/3322741/synchronizing-multiline-textbox-positions-in-c-sharp
+ */
+namespace isblTest
+{
+	public class SynchronizedScrollRichTextBox : System.Windows.Forms.RichTextBox
+	{
+		public SynchronizedScrollRichTextBox()
+		{
+			this.DoubleBuffered = true;
+		}
+        public int[] WM_EVENTS = { 0x115, 0x20A};
+		public int[] WM_NO_EVENTS = { 0x20, 0x200};
+		
+		private bool isScrollEvent(int eventCode)
+		{
+			foreach(int wm_eventCode in WM_EVENTS)
+			{
+				if(wm_eventCode == eventCode)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+		
+		private bool isNoScrollEvent(int eventCode)
+		{
+			foreach(int wm_eventCode in WM_NO_EVENTS)
+			{
+				if(wm_eventCode == eventCode)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		
+		private int eventNumber = 0;
+        List<SynchronizedScrollRichTextBox> peers = new List<SynchronizedScrollRichTextBox>();
+
+        public void AddPeer(SynchronizedScrollRichTextBox peer)
+        {
+            this.peers.Add(peer);
+        }
+
+        public void DirectWndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+			base.WndProc(ref m);
+			//System.Console.WriteLine(System.String.Format("{4}\tHWND:{3:X}, MSG:{2:X}, W:{0:X}, H:{1:X}", m.WParam, m.LParam, m.Msg, m.HWnd, eventNumber.ToString("00000")));
+			//eventNumber++;
+			/*
+            if (isScrollEvent(m.Msg))
+            {
+                foreach (var peer in this.peers)
+                {
+                    var peerMessage = Message.Create(peer.Handle, m.Msg, m.WParam, m.LParam);
+                    peer.DirectWndProc(ref peerMessage);
+                }
+            }
+            */
+        }
+	}
+}
